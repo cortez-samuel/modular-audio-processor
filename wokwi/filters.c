36ce0039011bcc18fc45f24b9351153f0a7cd98f
William Osborne <osbornew@ufl.edu>
2025-10-26 19:34:02 -0400

    filter.{c,h} for filtering

---
+++ wokwi/filters.c
@@ -0,0 +1,13 @@
+#include "filters.h"
+
+float low_pass(unsigned int *raw, unsigned int *filt, unsigned int index, float alpha)
+{
+	int prev = (unsigned int)(index - 1) % 128;
+	return ((1 - alpha) * (float)raw[index]) + (alpha * (float)filt[prev]);
+}
+
+float high_pass(unsigned int *raw, unsigned int *filt, unsigned int index, float alpha)
+{
+	int prev = (unsigned int)(index - 1) % 128;
+	return (alpha * (float)filt[prev]) + (alpha * ((float)raw[index] - raw[prev]));
+}
