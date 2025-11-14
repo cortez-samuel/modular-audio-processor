1bf4e714626ef654680e47a4c6fbc3e61584873b
Noah <2004.nbm@gmail.com>
2025-11-14 04:43:08 -0500

    updated ADC and checked (in separate project) if read() will also activate a FIFO_IRQ (it can since data is placed into FIFO, does not seem to be popped off FIFO on its own either)

--- libraries/adc.cpp
+++ libraries/adc.cpp
@@ -2,52 +2,110 @@
 
 #include "hardware/irq.h"
 
+// OBJECT STUFF
+ADC::ADC(uint8_t channel): value(0), channel(channel) {}
 
-void ADC::ADC_INIT() {
-    adc_init();
+void ADC::setRawValue(uint16_t raw){
+    value = raw;
 }
 
-ADC::ADC(uint8_t channel, float sample_freq_Hz) {
-    static const float ADC_CLK_HZ = 48 * 1000 * 1000;
-    adc_gpio_init(channel + 26);
-    adc_select_input(channel);
-
-    float div = ADC_CLK_HZ / sample_freq_Hz - 1.0f;
-    adc_set_clkdiv(div);
-}
-
-ADC::~ADC() {}
-
-void ADC::initFIFO(bool enable, bool dreq_enable, uint8_t threshold, bool shift, bool err) {
-    adc_fifo_setup(enable, dreq_enable, threshold, err, shift);
-}
-
-void ADC::initIRQ(irq_handler_t handler, bool enable) {
-    irq_set_exclusive_handler(ADC_IRQ_FIFO, handler);
-    adc_irq_set_enabled(enable);
-    irq_set_enabled(ADC_IRQ_FIFO, enable);
-}
-
-void ADC::clearFIFO() {
-    adc_fifo_drain();
-}
-
-void ADC::setActive() {
-}
-
-void ADC::readOnce()
+uint16_t ADC::rawValue() const
 {
-    value = adc_read();
-}
-void ADC::readContinuous(bool enable) {
-    adc_run(enable);
+    return value;
 }
 
 static const float conversion_factor = 3.3f / (1 << 12);
-uint16_t ADC::rawValue() const {
-    return value;
-}
 float ADC::trueValue() const {
     return value * conversion_factor;
 }
 
+uint8_t ADC::getChannel() const {
+    return channel;
+}
+
+// OVERALL ADC STUFF
+void ADC::init(float sample_freq, 
+        bool fifo_enable, bool dreq_enable, uint16_t threshold, 
+        irq_handler_t irq_handler) {
+
+    ADC::adcChannels[0] = ADC(0);
+    ADC::adcChannels[1] = ADC(1);
+    ADC::adcChannels[2] = ADC(2);
+    ADC::adcChannels[3] = ADC(3); 
+
+    ADC::setSampleFrequency(sample_freq);
+
+    ADC::setupFIFO(fifo_enable, dreq_enable, threshold);
+    ADC::setupIRQHandler(irq_handler);
+}
+
+ADC& ADC::getADCChannel(uint8_t channel) {
+    return adcChannels[channel];
+}
+
+ADC& ADC::getActiveChannel() {
+    return ADC::getADCChannel(activeChannel);
+}
+
+void ADC::setActiveChannel(uint8_t channel) {
+    activeChannel = channel;
+    adc_select_input(activeChannel);
+}
+
+void ADC::setActiveChannel(const ADC &adc) {
+    activeChannel = adc.channel;
+    adc_select_input(activeChannel);
+}
+
+uint16_t ADC::read() {
+    uint16_t raw = adc_read();
+    ADC::adcChannels[activeChannel].setRawValue(raw);
+    return raw;
+}
+
+uint16_t ADC::read(ADC& adc) {
+    // will also place data in FIFO buffer
+    ADC::setActiveChannel(adc);
+    return ADC::read();
+}
+
+void ADC::run(bool enable) {
+    adc_run(enable);
+    runningChannel = activeChannel;
+}
+void ADC::run(ADC& adc, bool enable) {
+    ADC::setActiveChannel(adc);
+    ADC::run(enable);
+}
+
+static const float ADC_CLK_HZ = 48 * 1000 * 1000;
+void ADC::setSampleFrequency(float sample_freq) {
+    float div = ADC_CLK_HZ / sample_freq - 1.0f;
+    adc_set_clkdiv(div);
+}
+
+void ADC::setupFIFO(bool enable, bool dreq_enable, uint16_t threshold) {
+    adc_fifo_setup(enable, dreq_enable, threshold, false, false);
+}
+void ADC::clearFIFO() {
+    adc_fifo_drain();
+}
+uint16_t ADC::readFIFO() {
+    uint16_t value = adc_fifo_get();
+    ADC::getActiveChannel().setRawValue(value);
+    return value;
+}
+
+void ADC::setupIRQHandler(irq_handler_t handler) {
+    irq_set_exclusive_handler(ADC_IRQ_FIFO, handler);
+}
+
+void ADC::enableIRQ(bool enable) {
+    adc_irq_set_enabled(enable);
+    irq_set_enabled(ADC_IRQ_FIFO, enable);
+}
+
+
+void defaultADCRIQHandler() {
+    ADC::readFIFO();
+}
--- libraries/adc.hpp
+++ libraries/adc.hpp
@@ -11,26 +11,49 @@ void defaultADCRIQHandler();
 
 class ADC {
 public:
-    static void ADC_INIT();
-    ADC(uint8_t channel, float sample_freq);
-    ~ADC();
+    static void init(float sample_freq, 
+        bool fifo_enable, bool dreq_enable, uint16_t threshold, 
+        irq_handler_t irq_handler);
+
+    static ADC& getADCChannel(uint8_t channel);
+
+    static ADC& getActiveChannel();
+    static void setActiveChannel(uint8_t channel);
+    static void setActiveChannel(const ADC& adc);
+
+    static uint16_t read();
+    static uint16_t read(ADC& adc);
+
+    static void run(bool enable);
+    static void run(ADC& adc, bool enable);
+
+    static void setSampleFrequency(float sample_freq);
+
+    static void setupFIFO(bool enable, bool dreq_enable, uint16_t threshold);
+    static void clearFIFO();
+    static uint16_t readFIFO();
+
+    static void setupIRQHandler(irq_handler_t handler);
+    static void enableIRQ(bool enable);
+private:
+    static uint8_t activeChannel;
+    static uint8_t runningChannel;
+
+    static ADC adcChannels[4];
 
 public:
-    void initFIFO(bool enable, bool dreq_enable, uint8_t threshold, bool shift = false, bool err = false); 
-    void initIRQ(irq_handler_t handler, bool enable);
-
-    void setActive();
-    void readOnce();
-    void readContinuous(bool enable);
+    ADC(uint8_t channel);
+    
+public:
+    void setRawValue(uint16_t raw);
 
     uint16_t rawValue() const;
     float trueValue() const;
 
-    void clearFIFO();
-
+    uint8_t getChannel() const;
 private:
     uint16_t value;
+    uint8_t channel;
 };
-
 
 #endif // ADC_HPP
