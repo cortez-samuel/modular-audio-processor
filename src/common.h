233202cc640e0428090120fd44fe2b9812d4f5ba
William Osborne <osbornew@ufl.edu>
2025-11-14 16:44:51 -0500

    init final "glue" code directory
    this will be for taking all the individual tests people have done and combining them together

---
+++ src/common.h
@@ -0,0 +1,9 @@
+#ifndef common_h_INCLUDED
+#define common_h_INCLUDED
+
+#define BUFFER_SIZE 512
+
+extern unsigned int BUFFER_SIGNAL[BUFFER_SIZE];
+extern unsigned int BUFFER_FILTER[BUFFER_SIZE];
+
+#endif // common_h_INCLUDED
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "common.h"
+#include "filters.h"
+
+int main(void)
+{
+    stdio_init_all();
+}
