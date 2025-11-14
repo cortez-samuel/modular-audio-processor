7c8f1a0974dc0084bb7967590bc5bedfd62a3f88
Noah McGlothlin <2004.nbm@gmail.com>
2025-11-14 12:51:48 -0500

    forgot to comment out my testing stuff

--- libraries/adc.cpp
+++ libraries/adc.cpp
@@ -127,10 +127,10 @@ void ADC::enableIRQ(bool enable) {
 }
 
 
-bool pin13 = true;
+//bool pin13 = true;
 #include <cstdio>
 void defaultADCRIQHandler() {
     ADC::readFIFO();
-    gpio_put(13, pin13);
-    pin13 = !pin13;
+    //gpio_put(13, pin13);
+    //pin13 = !pin13;
 }
